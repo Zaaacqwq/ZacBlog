51d836bc006766c72ae1b50c80db270a703f5759
Zaaacqwq <zacchenzy@gmail.com>
2025-01-20 20:46:24 -0500

    backend classes update

--- backend/zaaac-common/src/main/java/com/zaaac/common/config/ZaaacConfig.java
+++ backend/zaaac-common/src/main/java/com/zaaac/common/config/ZaaacConfig.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties(prefix = "zaaac")
-public class RuoYiConfig
+public class ZaaacConfig
 {
     /** 项目名称 */
     private String name;
@@ -80,7 +80,7 @@ public class RuoYiConfig
 
     public void setProfile(String profile)
     {
-        RuoYiConfig.profile = profile;
+        ZaaacConfig.profile = profile;
     }
 
     public static boolean isAddressEnabled()
@@ -90,7 +90,7 @@ public class RuoYiConfig
 
     public void setAddressEnabled(boolean addressEnabled)
     {
-        RuoYiConfig.addressEnabled = addressEnabled;
+        ZaaacConfig.addressEnabled = addressEnabled;
     }
 
     public static String getCaptchaType() {
@@ -98,7 +98,7 @@ public class RuoYiConfig
     }
 
     public void setCaptchaType(String captchaType) {
-        RuoYiConfig.captchaType = captchaType;
+        ZaaacConfig.captchaType = captchaType;
     }
 
     /**
